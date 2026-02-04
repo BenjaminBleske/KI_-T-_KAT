c9ed9e6da33b653fa2ba690a09cc414de8bba077
Benjamin Bleske <benjamin.bleske@hotmail.de>
2026-02-04 18:54:00 +0100

    Hier deine Nachricht

--- test_detection.py
+++ test_detection.py
@@ -8,7 +8,7 @@ from ultralytics import YOLO
 SOURCE = "rtsp://admin:KATZENKI199720192025@192.168.178.150:554/h264Preview_01_sub"
 DIR_PREVIEW = "annotated_previews"
 DIR_TRAINING = "raw_training_data"
-CONF_THRESHOLD = 0.40
+CONF_THRESHOLD = 0.60
 MIN_TIME_BETWEEN_SAVES = 5 
 HEARTBEAT_SECONDS = 10  # Feedback alle 10 Sekunden
 
